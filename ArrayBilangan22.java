84773962c29831a3f5bd3db682960d3a0a6fa8bd
valinadz <valinaadzra@gmail.com>
2023-10-30 10:07:43 +0700

    push

--- ArrayBilangan22.java
+++ ArrayBilangan22.java
@@ -1,16 +1,9 @@
 public class ArrayBilangan22 {
     public static void main(String[] args) {
-        int[] bil = new int [4];
+        int[] bil = {5.0, 12867, 7.5, 2000000};
 
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
-
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
-
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
